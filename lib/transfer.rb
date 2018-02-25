8c06906eca6d2c45cb32bbe761845136adcd2686
Neil Seejoor <nvseejoor@gmail.com>
2018-02-25 16:51:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
         @receiver.balance += @amount
         @status = "complete"
     else
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
     # @receiver.status = "complete"
